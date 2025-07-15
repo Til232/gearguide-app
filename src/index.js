8c592cbcce97b693afb0c78eb4f527855bef910b
Til232 <tilmann.schatz@gmail.com>
2025-07-15 11:21:57 +0200

    Hinzugefügt BrowserRouter mit basename für Routing-Fix auf GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Neu: Import für den Router
 import './index.css'; // Must be here
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/gearguide-app"> {/* Neu: Router mit basename für GitHub Pages */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
